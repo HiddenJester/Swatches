698121b217a24fb692b940cfa62e1bb41d506454
Timothy Sanders <tsanders@hiddenjester.com>
2019-11-12 17:53:06 -0800

    Added border to ColorChip.
    - Omitted if the chip is .clear.
    
    - .black border in light mode, UIColor.label in dark mode.

--- Swatches/Views/ColorChip.swift
+++ Swatches/Views/ColorChip.swift
@@ -11,6 +11,8 @@ import SwiftUI
 struct ColorChip: View {
     let color: Color
     
+    @Environment(\.colorScheme) var scheme
+    
     private let minSwatchSize = CGFloat(50)
     private let maxSwatchSize = CGFloat(300)
     
@@ -23,11 +25,26 @@ struct ColorChip: View {
             .aspectRatio(1.0, contentMode: .fit)
             .foregroundColor(color)
             .scaledToFit()
+            .overlay(RoundedRectangle(cornerRadius: 20.0).stroke(strokeColor()))
+    }
+    
+    func strokeColor() -> Color {
+        guard color != .clear else {
+            return .clear
+        }
+        
+        return scheme == .dark ? Color(UIColor.label) : .black
     }
 }
 
 struct ColorChip_Previews: PreviewProvider {
     static var previews: some View {
-        ColorChip(color: .green)
+        Group {
+            ColorChip(color: .green)
+            ColorChip(color: .green)
+                .previewDevice("iPhone SE")
+                .environment(\.colorScheme, .dark)
+            ColorChip(color: .clear)
+        }
     }
 }
