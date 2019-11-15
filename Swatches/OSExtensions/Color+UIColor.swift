364c4402d6b2287d8815ba68a034c99da8069f02
Timothy Sanders <tsanders@hiddenjester.com>
2019-11-15 12:16:58 -0800

    Restricted animation to colorScheme toggling.
    - The transition for selecting a new grid wasn’t the same in all cases. If the swatches had alpha (SwiftUI) then it would expand outwards from the center, but without alpha (Semantic) it would slide up from the bottom. Scoping the animation purely to the darkMode bool handles the problem.

---
+++ Swatches/OSExtensions/Color+UIColor.swift
@@ -0,0 +1,9 @@
+//
+//  Color+UIColor.swift
+//  Swatches
+//
+//  Created by Timothy Sanders on 2019-11-15.
+//  Copyright © 2019 HiddenJester Software. All rights reserved.
+//
+
+import Foundation
