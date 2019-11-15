20267902482d2df0337eb090f74281383cf4cab2
Timothy Sanders <tsanders@hiddenjester.com>
2019-11-14 18:36:48 -0800

    Created GridHeader.
    - Provides a segmented picker for selecting the grid to display.
    
    - Provides a toggle that controls dark/light mode.

---
+++ Swatches/Views/GridHeader.swift
@@ -0,0 +1,40 @@
+//
+//  GridHeader.swift
+//  Swatches
+//
+//  Created by Timothy Sanders on 2019-11-14.
+//  Copyright © 2019 HiddenJester Software. All rights reserved.
+//
+
+import SwiftUI
+
+struct GridHeader: View {
+    @Binding var darkModeSelected: Bool
+    
+    @Binding var gridIndex: Int
+    
+    @State var gridModels: [GridModel]
+    
+    var body: some View {
+        VStack {
+            Toggle("Dark Mode", isOn: $darkModeSelected)
+
+            Picker("Colors:", selection: $gridIndex) {
+                ForEach(0 ..< gridModels.count) { index in
+                    Text(self.gridModels[index].name).tag(index)
+                }
+            }.pickerStyle(SegmentedPickerStyle())
+
+            Divider()
+
+        }.padding()
+    }
+}
+
+struct GridHeader_Previews: PreviewProvider {
+    static var previews: some View {
+        GridHeader(darkModeSelected: .constant(false),
+                   gridIndex: .constant(0),
+                   gridModels: [GridModel(name: "SwiftUI", models: ColorModel.swiftUIColors())])
+    }
+}
