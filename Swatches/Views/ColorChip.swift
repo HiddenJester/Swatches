972c36229a96998c54c079387682eed15aff2e11
Timothy Sanders <tsanders@hiddenjester.com>
2019-11-25 17:21:47 -0800

    Commented the first batch of views.

--- Swatches/Views/ColorChip.swift
+++ Swatches/Views/ColorChip.swift
@@ -8,15 +8,25 @@
 
 import SwiftUI
 
+/// A view that draws a color "chip", potentially with a checkerboard background. The chip will will accept any size between 50 points and 300 points, but
+/// will maintain a square aspect ratio. The color drawn is specified in the `color` argument, and `drawBackground` controls whether the checkerboard
+/// and outline are drawn or not. The color chip is drawn with rounded corners.
+/// - Note: `ColorChip(color: .clear, drawBackground: false)` creates a transparent chip (*not* an `EmptyView()`) that is sized as
+///     described above.
 struct ColorChip: View {
+    /// The color to draw in the chip.
     let color: Color
     
+    /// Set to true to draw the checkerboard background (and the outline).
     let drawBackground: Bool
     
+    /// The minimum dimension a chip will accept.
     private let minChipEdge = CGFloat(50)
     
+    /// The maxium dimension a chip will accept.
     private let maxChipEdge = CGFloat(300)
     
+    /// The corner radius used for the Chip and the background.
     private let cornerRadius = CGFloat(20.0)
     
     var body: some View {
--- Swatches/Views/ColorSwatch.swift
+++ Swatches/Views/ColorSwatch.swift
@@ -8,7 +8,11 @@
 
 import SwiftUI
 
+/// A view that draws everything needed for a given `ColorModel`. A `Swatch` is created and the body of the `Swatch` will be a `ColorChip` object
+/// representing the color in the model. If nil is provided for the model then an empty `Swatch` will be created, containing a transparent `ColorChip` with
+/// no background.
 struct ColorSwatch: View {
+    /// The `ColorModel` that will be rendered in this swatch.
     let model: ColorModel?
     
     
