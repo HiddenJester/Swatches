f42e98332076912b41335b4b2d809b504f28b636
Timothy Sanders <tsanders@hiddenjester.com>
2020-01-06 15:43:06 -0800

    Added link to the colors gist.

--- Common/Views/AboutView.swift
+++ Common/Views/AboutView.swift
@@ -11,20 +11,31 @@ import SwiftUI
 struct AboutView: View {
     @Environment (\.presentationMode) var presentationMode
 
-    private let bodyText =
-    """
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-"""
+    @State private var showGist = false
+    
+    private let gistURL: URL = URL(string: "https://gist.github.com/HiddenJester/1a601bc5256dccaa0022bcd973a76c8b")!
+
     var body: some View {
         VStack {
             Text("About Swatches")
                 .font(Font.title)
             
-            Spacer()
+            Text("Swatches displays all of the colors defined in both SwiftUI and UIKit. There is an extension for SwiftUI.Color that maps all of the UIKit colors, allowing you to write code like:")
+                .padding()
             
-            Text(bodyText)
-            
-            Spacer()
+            CodeSampleView(text: "RoundedRectangle.backgroundColor(.systemGray4)")
+
+            #if os(tvOS) // No web view for tvOS, just show the url in the link color.
+            CodeSampleView(text: gistURL.absoluteString, foregroundColor: .link, backgroundColor: .clear)
+                .padding()
+            #else // Provide a button that opens the gist in a SafariView.
+            Button(action: { self.showGist.toggle() }) {
+                CodeSampleView(text: gistURL.absoluteString, foregroundColor: .link, backgroundColor: .clear)
+            }.padding(.vertical)
+                .sheet(isPresented: self.$showGist) {
+                    SafariView(url: self.gistURL)
+            }
+            #endif
             
             Button(action: { self.presentationMode.wrappedValue.dismiss() }) {
                 Text("Dismiss")
@@ -33,6 +44,8 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor i
         }
     }
 }
+
+
 
 struct AboutView_Previews: PreviewProvider {
     static var previews: some View {
