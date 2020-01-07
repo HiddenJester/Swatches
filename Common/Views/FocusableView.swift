dc26bff9aadecc566c1349e07192da91dc225a5f
Timothy Sanders <tsanders@hiddenjester.com>
2020-01-07 15:28:59 -0800

    Renamed FocusableGridRow➡️FocusableView
    - Tweaked layout slightly.
    
    - Added the ability to specify a custom focus scale value.

--- Common/Views/FocusableView.swift
+++ Common/Views/FocusableView.swift
@@ -8,55 +8,59 @@
 
 import SwiftUI
 
-struct FocusableRowView<Content>: View where Content: View {
+struct FocusableView<Content>: View where Content: View {
+    /// The amount to scale the view when it has focus
+    let focusScale: CGFloat
+    
     /// Content passed in to render.
     private let content: Content
-
+    
     /// When the content has focus, scale it up by this value in order to see where the focus is.
     @State private var scale = CGFloat(1)
     
     var body: some View {
         platformSpecificView()
-
+        
     }
-
-    init(@ViewBuilder _ content: @escaping () -> Content) {
+    
+    init(focusScale: CGFloat = 1.15, @ViewBuilder _ content: @escaping () -> Content) {
+        self.focusScale = focusScale
         self.content = content()
     }
 }
 
-private extension FocusableRowView {
+private extension FocusableView {
     /// Create the platform-specific view. Although everything but iOS *supports* focusable, we only want to use it on tvOS. On all other platforms this
     /// just returns `self.content`. On tvOS `self.content` is centered in a `HStack`, then padded and scaled up by `self.scale`. A
     /// `focusable` modifier is added to adjust `self.scale`.
     /// - Returns: `self.content`. If needed by the platform it is wrapped in the needed modifiers to implement focusable support & display.
     func platformSpecificView() -> some View {
         #if os(tvOS)
-        return HStack {
-            Spacer()
-            
-            self.content
-                .padding()
-                .scaleEffect(self.scale)
-                .focusable(true) { focus in withAnimation { self.scale = focus ? 1.05 : 1 } }
-            
-            Spacer()
-        }
+        return self.content
+            .scaleEffect(self.scale)
+            .padding()
+            .focusable() { focus in self.scale = focus ? self.focusScale : 1 }
+        
         #else
         return content
         #endif
     }
 }
-struct FocusableRowView_Previews: PreviewProvider {
+struct FocusableView_Previews: PreviewProvider {
     static var models = ColorModel.swiftUIColors()
-
+    
     static var previews: some View {
-        FocusableRowView() {
-            HStack {
-                ColorSwatchView(model: models[0], maxWidth: 150)
-
-                ColorSwatchView(model: models[1], maxWidth: 150)
+        HStack {
+            FocusableView() {
+                ColorSwatchView(model: models[0], maxWidth: 350)
+                
             }
+            
+            FocusableView() {
+                ColorSwatchView(model: models[1], maxWidth: 350)
+                
+            }
+            
         }
     }
 }
