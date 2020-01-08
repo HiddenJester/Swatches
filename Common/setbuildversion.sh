a17577ebdbd7b5e6eade14010d93bc42aaa81d8a
Timothy Sanders <tsanders@hiddenjester.com>
2020-01-08 15:47:47 -0800

    Added setbuildversion.sh for iOS & tvOS.

---
+++ Common/setbuildversion.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Copied from http://blog.jaredsinclair.com/post/97193356620/the-best-of-all-possible-xcode-automated-build
+
+# Set the build number to the current git commit count.
+# If we're using the Dev scheme, then we'll suffix the build
+# number with the current branch name, to make collisions
+# far less likely across feature branches.
+#   http://w3facility.info/question/how-do-i-force-xcode-to-rebuild-the-info-plist-file-in-my-project-every-time-i-build-the-project/
+#
+git=`sh /etc/profile; which git`
+appBuild=`"$git" rev-list --all |wc -l`
+if [ $CONFIGURATION = "Debug" ]; then
+branchName=`"$git" rev-parse --abbrev-ref HEAD`
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $appBuild-$branchName" "${TARGET_BUILD_DIR}/${INFOPLIST_PATH}"
+else
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $appBuild" "${TARGET_BUILD_DIR}/${INFOPLIST_PATH}"
+fi
+echo "Updated ${TARGET_BUILD_DIR}/${INFOPLIST_PATH}"
