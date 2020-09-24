219ae9d521eb7a5db486f39880a064890da800ec
Timothy Sanders <tsanders@hiddenjester.com>
2020-09-24 14:25:29 -0700

    Added WatchMainView.
    - Decided I’d rather implement a View and get rid of the AnyView wrapper.
    
    - Also needed to write HeaderDocs for colorListView(models:).
    
    - Added a “Color Sets” Title row to the initial screen.

--- watchOS Extension/HostingController.swift
+++ watchOS Extension/HostingController.swift
@@ -10,20 +10,39 @@ import WatchKit
 import Foundation
 import SwiftUI
 
-class HostingController: WKHostingController<AnyView> {
-    override var body: AnyView {
-        AnyView(List {
-            NavigationLink("SwiftUI", destination: ColorListView(models: ColorModel.swiftUIColors()))
-            NavigationLink("Fixed", destination: ColorListView(models: ColorModel.fixedColors()))
-        })
-    }
+/// A very thin shim to get around `WKHostingController` needing the type of the view. I mean, the type is known, but it's super-ugly & brittle that's the
+/// whole point of the `some View` construction in the first place. The alternative is to use `AnyView`, but this looks less hacky.
+struct WatchMainView: View {
+    var body: some View {
+        VStack {
+            Text("Color Sets")
+                .font(.title)
 
-    func ColorListView(models: [ColorModel]) -> some View {
+            List {
+                NavigationLink("SwiftUI", destination: colorListView(models: ColorModel.swiftUIColors()))
+                NavigationLink("Fixed", destination: colorListView(models: ColorModel.fixedColors()))
+            }
+        }
+    }
+}
+
+private extension WatchMainView {
+    /// Creates the watch-specific scrollable view of color models. Since none of the text colors are supported on the watch we don't need to through
+    /// the indirection of templating for `GridModel` and can directly use `ColorModel` here.
+    /// - Parameter models: The `ColorModel` objects to render
+    /// - Returns: A simple `ScrollView` showing all of the `ColorSwatchViews` for the provided models.
+    func colorListView(models: [ColorModel]) -> some View {
         ScrollView {
             ForEach(models, id: \.self) { (model) in
                 ColorSwatchView(model: model, width: nil)
             }
         }
+    }
+}
+
+class HostingController: WKHostingController<WatchMainView> {
+    override var body: WatchMainView {
+        WatchMainView()
     }
 }
 
