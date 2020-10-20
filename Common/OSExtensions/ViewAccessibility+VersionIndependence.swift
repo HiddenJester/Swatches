376266457002e81f4cd4bf7ee1c2afb5e3287322
Timothy Sanders <tsanders@hiddenjester.com>
2020-10-20 12:09:35 -0700

    Forced macCatalyst to use iOS 13 accessibility mods.
    The if #available tests are not compiling properly on the Mac target, so force that version to use the older acceessbility modifiers.

--- Common/OSExtensions/ViewAccessibility+VersionIndependence.swift
+++ Common/OSExtensions/ViewAccessibility+VersionIndependence.swift
@@ -9,17 +9,25 @@
 import SwiftUI
 
 /// Provides iOS 13/14 (and matching watchOS or tvOS where appropriate) version independence for accessibility modifiers.
+/// - NOTE: the macCatalyst version of these are broken. We're targeting iOS 13 as a deploy target, but the #available checks below incorrectly claim that
+///     macCatalyst 13 supports the new accessibilityFoo functions which are only available if we are running on macOS 11.0. So these functions all use the iOS
+///     13 calls when on the macCatalyst target, without using the `#available` check..
 extension View {
     /// Wraps iOS 14 `accessibilityLabel` & iOS 13 `accessibility(label:)` into a single function that takes a string and applies the proper
     /// modifier.
     /// - Parameter labelString: Something that conforms to `StringProtocol` that will be applies as the accessibility label.
     /// - Returns: The modified view.
     func accessibility<S>(labelString: S) -> ModifiedContent<Self, AccessibilityAttachmentModifier> where S: StringProtocol {
+        #if targetEnvironment(macCatalyst)
+        // See comment at top of extension for justifying this.
+        return self.accessibility(label: Text(labelString))
+        #else
         if #available(iOS 14, watchOS 7, tvOS 14, *) {
             return self.accessibilityLabel(labelString)
         } else {
             return self.accessibility(label: Text(labelString))
         }
+        #endif
     }
 
     /// Wraps iOS 14 `accessibilityHint` & iOS 13 `accessibility(hint:)` into a single function that takes a string and applies the proper
@@ -27,11 +35,16 @@ extension View {
     /// - Parameter labelString: Something that conforms to `StringProtocol` that will be applies as the accessibility hint.
     /// - Returns: The modified view.
     func accessibility<S>(hintString: S) -> ModifiedContent<Self, AccessibilityAttachmentModifier> where S: StringProtocol {
+        #if targetEnvironment(macCatalyst)
+        // See comment at top of extension for justifying this.
+        return self.accessibility(hint: Text(hintString))
+        #else
         if #available(iOS 14, watchOS 7, tvOS 14, *) {
             return self.accessibilityHint(hintString)
         } else {
             return self.accessibility(hint: Text(hintString))
         }
+        #endif
     }
 
     /// Wraps iOS 14 `accessibilitySortPriority` & iOS 13 `accessibility(sortPriority:)` into a single function that takes a `Double` and
@@ -39,11 +52,16 @@ extension View {
     /// - Parameter sortPriority: The sort priority to apply to the content.
     /// - Returns: The modified view.
     func accessibilitySort(priority: Double) -> ModifiedContent<Self, AccessibilityAttachmentModifier> {
+        #if targetEnvironment(macCatalyst)
+        // See comment at top of extension for justifying this.
+        return self.accessibility(sortPriority: priority)
+        #else
         if #available(iOS 14, watchOS 7, tvOS 14, *) {
             return self.accessibilitySortPriority(priority)
         } else {
             return self.accessibility(sortPriority: priority)
         }
+        #endif
     }
 
     /// Wraps iOS 14 `accessibilityHidden` & iOS 13 `accessibility(hidden:)` into a single function that takes a bool and
@@ -51,10 +69,15 @@ extension View {
     /// - Parameter hideStatus: The bool that controls whether the view is hidden from accessibility.
     /// - Returns: The modified view.
     func accessibilityHide(hideStatus: Bool) -> ModifiedContent<Self, AccessibilityAttachmentModifier> {
+        #if targetEnvironment(macCatalyst)
+        // See comment at top of extension for justifying this.
+        return self.accessibility(hidden: hideStatus)
+        #else
         if #available(iOS 14, watchOS 7, tvOS 14, *) {
             return self.accessibilityHidden(hideStatus)
         } else {
             return self.accessibility(hidden: hideStatus)
         }
+        #endif
     }
 }
